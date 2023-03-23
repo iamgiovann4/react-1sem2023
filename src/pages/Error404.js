befa81c3dadd1b0cc8eb2e2afddef0019774eefe
Giovanna <giovannasiqueiranicolau@gmail.com>
2023-03-23 17:48:02 -0300

    create page 404

---
+++ src/pages/Error404.js
@@ -0,0 +1,26 @@
+import Box from '@mui/material/Box'
+import Header from '../components/Header'
+import MainMenu from '../components/MainMenu'
+import Sidebar from '../components/Sidebar'
+import Footer from '../components/Footer'
+
+const Error404 = () => {
+  return (
+    <>
+      <Header />
+      <MainMenu />
+      <Box sx={{
+        display: 'flex'
+      }}>
+        <Sidebar />
+        <Box sx={{padding: "25px"}}>
+            <h1>Opsss... Página não encontrada!</h1>
+            <h4>Verifique se a URL está correta.</h4>
+        </Box>
+      </Box>
+      <Footer />
+    </>
+  )
+}
+
+export default Error404
