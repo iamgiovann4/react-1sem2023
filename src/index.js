d75a7ec2daf27da2f67c0336edeeabaf7d175b9c
Giovanna <giovannasiqueiranicolau@gmail.com>
2023-03-23 17:23:30 -0300

    add browser router from react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client'; //biblioteca q o react forneceu
-// import './index.css'; //arquivos css importam sem o from
-// import App from './App'; //importei o componente. //arquivos js importam com from sem .js
-
-// const root = ReactDOM.createRoot(document.getElementById('root')); // Criando uma div com ID root
-// root.render( //render significa renderizar, ou seja, exibir dentro de root tais coisas.
-//   <React.StrictMode>
-//     {/* <div>Olá Mundo</div> */}
-//     <App/> {/*Forma de usar componentes externos que foram importados*/}
-//   </React.StrictMode>
-// );
