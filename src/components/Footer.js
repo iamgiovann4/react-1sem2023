5284b8b06857cd9d4f009e71160a278e0554207c
Giovanna <giovannasiqueiranicolau@gmail.com>
2023-04-14 17:10:11 -0300

    edit the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,18 @@ import Box from '@mui/material/Box'
 
 const Footer = () => {
     return(
-        <Box sx={{
-            background: 'salmon',
-            textAlign: 'center',
-            padding: '10px'
-        }}>
+        <Box component="footer" sx={styles.footer}>
            <p>Giovanna Siqueira Nicolau</p>
        </Box>
     )
+}
+
+const styles = {
+    footer: {
+        background: '#555',
+        color: '#000',
+        padding: '20px',
+        marginTop: '5px'
+    }
 }
 export default Footer
